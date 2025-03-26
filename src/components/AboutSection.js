a8599a7a704e6bb2fda9120d208c967249ce2aa5
Michelle Clement <michellelclement@gmail.com>
2025-03-26 13:07:28 +0000

    Add styling to services

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png'
 
 // Styles
 import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
@@ -28,37 +29,5 @@ const AboutSection = () => {
     </About>
   );
 }
-
-//Styled components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden; // This will hide the image that goes outside the div
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover; // This will make the image cover the whole div
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
