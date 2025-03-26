f1999735d10fc91f762c6226377404b4ca90f89f
Michelle Clement <michellelclement@gmail.com>
2025-03-26 11:24:28 +0000

    Add Services

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,15 @@ import React from "react";
 
 //Page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-    return (
-       <AboutSection /> 
-    );
+  return (
+    <>
+      <AboutSection /> 
+      <ServicesSection />
+    </>
+  );
 }
 
 export default AboutUs;
